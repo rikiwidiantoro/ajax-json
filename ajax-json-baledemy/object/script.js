16f0fecaf5aee8965192dc5b689466a498a1259a
Riki-Toro <rikitoro22@gmail.com>
2021-09-04 16:54:39 +0700

    tipe data json

---
+++ ajax-json-baledemy/object/script.js
@@ -0,0 +1,11 @@
+const Orang = {
+    nama : "Riki Widiantoro",
+    umur : 22,
+    sudahMakan : false,
+    skills : ['html', 'css', 'javascript'],
+    alamat : {
+        desa : 'belang',
+        kecamatan : 'bungkal',
+        kawin : false
+    }
+}
