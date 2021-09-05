2f434b2780448ff9849b8e2301f80123218e5200
Riki-Toro <rikitoro22@gmail.com>
2021-09-05 18:23:35 +0700

    update

--- ajax-json-baledemy/latihan-ajax-2/data.js
+++ ajax-json-baledemy/latihan-ajax-2/data.js
@@ -10,3 +10,24 @@ console.log(Hero);
 
 // merubah object menjadi json dengan JSON.stringify()
 console.log(JSON.stringify(Hero));
+
+// merubah json menjadi object dengan JSON.parse()
+// json vanila javascript/ javascript murni
+const ajax = new XMLHttpRequest();
+
+ajax.open('GET', '../buat-json/aot.json', true);
+
+ajax.onreadystatechange = function() {
+    if(ajax.readyState === 4 && ajax.status === 200) {
+        // merubah json menjadi object
+        console.log(JSON.parse(this.responseText));
+    }
+}
+
+ajax.send();
+
+console.log('--------------------------')
+
+// ajax dengan jQuery
+
+
