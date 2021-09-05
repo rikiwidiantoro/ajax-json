4988e508a5aa8a3dc170a6d1268a8a8601c920ea
Riki-Toro <rikitoro22@gmail.com>
2021-09-05 18:11:07 +0700

    json stringify

--- ajax-json-baledemy/latihan-ajax-2/data.js
+++ ajax-json-baledemy/latihan-ajax-2/data.js
@@ -5,3 +5,8 @@ const Hero = {
         'mahasiswa', 'frontend developer', true
     ]
 }
+
+console.log(Hero);
+
+// merubah object menjadi json dengan JSON.stringify()
+console.log(JSON.stringify(Hero));
