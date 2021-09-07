22cca1288587a2222e1fd3c0aefd175a0c35c6b1
Riki-Toro <rikitoro22@gmail.com>
2021-09-07 15:22:06 +0700

    mengambil data dengan jquery

--- ajax-json-baledemy/latihan-web-project-jco/script.js
+++ ajax-json-baledemy/latihan-web-project-jco/script.js
@@ -28,6 +28,39 @@ $(document).ready(function() {
 
         // console.log(datas.menu[1].deskripsi); >> cara detail
 
+        $.each(datas, function(index, menus) {
+
+            // // console.log(index);
+            // console.log(menus[1].nama);
+            $.each(menus, function(index, menu) {
+                // console.log(menu.nama);
+
+
+
+                $('.row1').html(
+                    `
+                        <div class="col-sm-4 text-center mt-2">
+                            <div class="card">
+                                <div class="card-header">
+                                    ${menu.nama}
+                                </div>
+                                <div class="card-body">
+                                    <img src="../images/${menu.gambar}" alt="${menu.nama}" width="200">
+                                    <p><strong class="badge badge-warning"><h5>${menu.kategori}</h5></strong></p>
+                                    <p class="card-text">${menu.deskripsi}</p>
+                                    <a href="#" class="btn btn-primary">See Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    `
+                ) 
+
+
+
+            });
+
+        });
+
 
     });
 
