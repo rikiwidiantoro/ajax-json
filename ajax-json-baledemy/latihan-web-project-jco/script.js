2abcc9a22acbdfe37b6452e495770509111e9af6
Riki-Toro <rikitoro22@gmail.com>
2021-09-07 14:57:25 +0700

    jco donuts

--- ajax-json-baledemy/latihan-web-project-jco/script.js
+++ ajax-json-baledemy/latihan-web-project-jco/script.js
@@ -1,19 +1,34 @@
-// javascript biasa
-function ajx() {
-    let ajax = new XMLHttpRequest();
+// // javascript biasa
+// function ajx() {
+//     let ajax = new XMLHttpRequest();
 
-    ajax.open('GET', 'data/donuts.json', true);
+//     ajax.open('GET', 'data/donuts.json', true);
 
-    ajax.onreadystatechange = function() {
-        if( ajax.readyState === 4 && ajax.status === 200 ) {
-            let data = JSON.parse(this.responseText);
+//     ajax.onreadystatechange = function() {
+//         if( ajax.readyState === 4 && ajax.status === 200 ) {
+//             let data = JSON.parse(this.responseText);
 
-            console.log(data.menu[0].nama);
+//             console.log(data.menu[0].nama);
 
-        }
-    }
+//         }
+//     }
 
-    ajax.send();
-}
+//     ajax.send();
+// }
 
-ajx();
+// ajx();
+
+
+
+// jQuery
+$(document).ready(function() {
+
+    // mengambil data dari json
+    $.getJSON('data/donuts.json', function(datas) {
+
+        // console.log(datas.menu[1].deskripsi); >> cara detail
+
+
+    });
+
+});
